d6031c1cd751eded71640f3e34a186642b151b21
anasfurqan250577 <k250577@nu.edu.pk>
2025-11-14 21:19:02 +0500

    Update move-zeroes.c

--- move-zeroes.c
+++ move-zeroes.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    int nums[n], nzp=0;
-    for(int i=0; i<n; i++){
-        scanf("%d", &nums[i]);
-    }
-    
-    for(int i=0; i<n; i++){
+void moveZeroes(int *nums, int size){
+    int nzp=0;
+    for(int i=0; i<size; i++){
         if(nums[i]!=0){
             nums[nzp] = nums[i];
             nzp++;
         }
     }
     
-    while(nzp!=(n)) {
+    while(nzp!=(size)) {
         nums[nzp] = 0;
         nzp++;
     }
     
-    for(int i=0; i<n; i++) printf("%d ", nums[i]);
-    
+    for(int i=0; i<size; i++) printf("%d ", nums[i]);
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    for(int i=0; i<n; i++) scanf("%d", &nums[i]);
+    moveZeroes(nums, n);
     return 0;
 }
